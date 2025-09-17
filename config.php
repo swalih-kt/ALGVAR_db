7610421b6f0eceb09d7fe566152f47a5d16e5fe4
swalihkt <swalihkt18751875@gmail.com>
2025-09-17 13:05:34 +0530

    second

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'alg_cdg');
-define('MAX_SEARCH_LENGTH', 100);
+define('MAX_SEARCH_LENGTH', 150);
 ?>
